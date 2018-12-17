c5a1975c5bfcab920cbfbfd91ec91c40e3d3c9a6
3a532043 <s3a532043@student.ncut.edu.tw>
2018-12-17 17:55:35 +0800

    建立子樣板 將index.php重新命名為index.blade.php 並編輯其內容

--- resources/views/home/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2018/12/17
- * Time: 下午 05:29
- */
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,6 @@
 */
 
 Route::get('/', function () {
-    return view('layouts.master');
+    return view('home.index');
 });
 Route::get('/home','HomeController@index');
